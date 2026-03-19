df3e2f592d2e8919ffe0f5770b7d77e8d44dce5a
Lueezyy <luis.balb@icloud.com>
2026-03-19 16:11:19 -0500

    Fixed java code

--- linearSearch.java
+++ linearSearch.java
@@ -1,5 +1,4 @@
 import java.util.Random;
-import java.util.Scanner;
 import java.util.Arrays;
 
 public class linearSearch{
@@ -13,27 +12,19 @@ public class linearSearch{
         return new int[] {-1, checks};
     }
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
         Random random = new Random();
 
-        System.out.print("Repeat test how many times: ");
-        int rep = input.nextInt();
-        input.close();
+        int[] arr = new int[25];
+        for (int j=0; j<25; j++)
+            arr[j] = random.nextInt(25) + 1;
 
-        for (int i=0; i<rep; i++){
-            int[] arr = new int[50];
-            for (int j=0; j<50; j++)
-                arr[j] = random.nextInt(50) + 1;
+        System.out.println(Arrays.toString(arr));
 
-            System.out.println(Arrays.toString(arr));
+        int sn = random.nextInt(25) + 1;
+        int[] result = linear_search(arr, sn);
+        int index = result[0];
+        int checks = result[1];
 
-            int sn = random.nextInt(50) + 1;
-            int[] result = linear_search(arr, sn);
-            int index = result[0];
-            int checks = result[1];
-
-            System.out.printf("Search number: %d | Linear search: %d elements checked\n", sn, checks);
-            System.out.println();
-        }
+        System.out.printf("Search number: %d \nIndex: %d \nLinear search: %d elements checked\n\n", sn, index, checks);
     }
 }
